abef6c93984d762edc7b6e01fe0184aff70e0698
haykam821 <24855774+haykam821@users.noreply.github.com>
2021-06-30 14:52:48 -0400

    Fix ESLint errors

--- src/main.ts
+++ src/main.ts
@@ -3,23 +3,23 @@ import { getOctokit } from "@actions/github";
 import { yarnUpdateBase } from "./yarn-update-base";
 
 async function main(): Promise<void> {
-  const token = core.getInput("github-token", { required: true });
-  const context = core.getInput("context", { required: true });
+	const token = core.getInput("github-token", { required: true });
+	const context = core.getInput("context", { required: true });
 
-  const github = getOctokit(token, {});
+	const github = getOctokit(token, {});
 
-  console.log("Fabric Actions Script, context: " + context);
+	console.log("Fabric Actions Script, context: " + context);
 
-  switch (context) {
-    case "yarn-update-base":
-      yarnUpdateBase(
-        github.rest,
-        parseInt(core.getInput("issue-number", { required: true }))
-      );
-      break;
-    default:
-      throw new Error("Unknown context: " + context);
-  }
+	switch (context) {
+		case "yarn-update-base":
+			await yarnUpdateBase(
+				github.rest,
+				parseInt(core.getInput("issue-number", { required: true })),
+			);
+			break;
+		default:
+			throw new Error("Unknown context: " + context);
+	}
 }
 
 main();
--- src/yarn-update-base.ts
+++ src/yarn-update-base.ts
@@ -1,84 +1,97 @@
-import { context } from "@actions/github";
 import { RestEndpointMethods } from "@octokit/plugin-rest-endpoint-methods/dist-types/generated/method-types";
+import { context } from "@actions/github";
 
 export async function yarnUpdateBase(
-  github: RestEndpointMethods,
-  issue_number: number
-) {
-  const owner = context.repo.owner;
-  const repo = context.repo.repo;
+	github: RestEndpointMethods,
+	issue_number: number,
+): Promise<void> {
+	const owner = context.repo.owner;
+	const repo = context.repo.repo;
 
-  const labels = await getLabels(github, issue_number);
+	const labels = await getLabels(github, issue_number);
 
-  if (!labels.has("update-base")) {
-    console.log("Nothing to do");
-    return;
-  }
+	if (!labels.has("update-base")) {
+		console.log("Nothing to do");
+		return;
+	}
 
-  const baseRequest = { owner, repo };
-  const pullRequest = { ...baseRequest, pull_number: issue_number };
-  const issueRequest = { ...baseRequest, issue_number };
+	const baseRequest = {
+		owner,
+		repo,
+	};
+	const pullRequest = {
+		...baseRequest,
+		pull_number: issue_number,
+	};
+	const issueRequest = {
+		...baseRequest,
+		issue_number,
+	};
 
-  const { data: repoData } = await github.repos.get({ ...baseRequest });
-  const base = repoData.default_branch;
-  const { data: pull } = await github.pulls.get({
-    ...pullRequest,
-  });
-  labels.delete("update-base");
+	const { data: repoData } = await github.repos.get({ ...baseRequest });
+	const base = repoData.default_branch;
+	const { data: pull } = await github.pulls.get({
+		...pullRequest,
+	});
+	labels.delete("update-base");
 
-  if (pull.base.ref == base) {
-    await github.issues.createComment({
-      ...issueRequest,
-      body: "🚨 Target branch is already set to " + base,
-    });
-  } else {
-    // Update the base (target) branch
-    await github.pulls.update({ ...pullRequest, base });
-    await github.issues.createComment({
-      ...issueRequest,
-      body: "🚀 Target branch has been updated to " + base,
-    });
+	if (pull.base.ref == base) {
+		await github.issues.createComment({
+			...issueRequest,
+			body: "🚨 Target branch is already set to " + base,
+		});
+	} else {
+		// Update the base (target) branch
+		await github.pulls.update({
+			...pullRequest,
+			base,
+		});
+		await github.issues.createComment({
+			...issueRequest,
+			body: "🚀 Target branch has been updated to " + base,
+		});
 
-    try {
-      // Updates the pull request with the latest upstream changes.
-      await github.pulls.updateBranch({ ...pullRequest });
-    } catch (error) {
-      // 422 is returned when there is a merge conflict
-      if (error.status === 422) {
-        await github.issues.createComment({
-          ...issueRequest,
-          body: "🚨 Please fix merge conflicts before this can be merged",
-        });
+		try {
+			// Updates the pull request with the latest upstream changes.
+			await github.pulls.updateBranch({ ...pullRequest });
+		} catch (error) {
+			// 422 is returned when there is a merge conflict
+			if (error.status === 422) {
+				await github.issues.createComment({
+					...issueRequest,
+					body: "🚨 Please fix merge conflicts before this can be merged",
+				});
 
-        labels.add("outdated");
-      } else {
-        throw error;
-      }
-    }
+				labels.add("outdated");
+			} else {
+				throw error;
+			}
+		}
 
-    labels.delete("snapshot");
-    labels.delete("release");
+		labels.delete("snapshot");
+		labels.delete("release");
 
-    const snapshot = base.includes("-") || base.includes("w");
-    labels.add(snapshot ? "snapshot" : "release");
-  }
+		const snapshot = base.includes("-") || base.includes("w");
+		labels.add(snapshot ? "snapshot" : "release");
+	}
 
-  const distictLabels: string[] = [...labels];
-  await github.issues.setLabels({
-    ...issueRequest,
-    labels: distictLabels as any, // TODO are the types wrong here or something?
-  });
+	const distictLabels: string[] = [...labels];
+	await github.issues.setLabels({
+		...issueRequest,
+		// TODO are the types wrong here or something?
+		labels: distictLabels as never,
+	});
 }
 
 async function getLabels(
-  github: RestEndpointMethods,
-  issue_number: number
+	github: RestEndpointMethods,
+	issue_number: number,
 ): Promise<Set<string>> {
-  const resp = await github.issues.listLabelsOnIssue({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    issue_number,
-  });
-  const data = await resp.data;
-  return new Set(data.map((label) => label.name));
+	const resp = await github.issues.listLabelsOnIssue({
+		issue_number,
+		owner: context.repo.owner,
+		repo: context.repo.repo,
+	});
+	const data = await resp.data;
+	return new Set(data.map(label => label.name));
 }
